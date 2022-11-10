bcf267c943ce67ab5e1d4f50d4babccb52c5c7bf
NidhiSingh6 <109155564+NidhiSingh6@users.noreply.github.com>
2022-11-10 23:54:59 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 
 
 const Config = {
-    IP: 'http://localhost:3333',
-    // IP:'http://34.221.140.213:3333',
+    // IP: 'http://localhost:3333',
+    IP:'http://34.221.140.213:3333',
 
     email: 'nidhi6211si@gmail.com',
     phone: `+918303434501`,
